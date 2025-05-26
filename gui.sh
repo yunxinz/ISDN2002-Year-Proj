607eb66ac5d4ff676f99eabad60f2ce54b0b4ae0
yunxinz <zhang.yunxin@live.com>
2025-05-26 23:31:52 +0800

    add missing stop nav logic for qt gui + auto format + add gui launch script

--- gui.sh
+++ gui.sh
@@ -1,7 +1,9 @@
 # # build for the qt_gui_ros2 package
-# source /opt/ros/humble/setup.bash
-# colcon build --packages-select qt_gui_ros2
+source /opt/ros/humble/setup.bash
+colcon build --packages-select qt_gui_ros2
 
 # run the qt_gui_ros2 package
 source /home/ada/ISDN2002-Year-Proj/install/setup.bash
 ros2 run qt_gui_ros2 qt_gui_ros2
+
+
--- src/yp_menu_gui/qt_gui_ros2/src/main.cpp
+++ src/yp_menu_gui/qt_gui_ros2/src/main.cpp
@@ -1,21 +1,23 @@
 #include <QApplication>
 #include <memory>
+
 #include "../include/qt_gui_ros2/mainWindow.hpp"
 #include "rviz_common/ros_integration/ros_node_abstraction.hpp"
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    rclcpp::init(argc, argv);
+  QApplication app(argc, argv);
+  rclcpp::init(argc, argv);
 
-    auto ros_node_abs = std::make_shared<rviz_common::ros_integration::RosNodeAbstraction>("rviz_render_node");
+  auto ros_node_abs =
+      std::make_shared<rviz_common::ros_integration::RosNodeAbstraction>(
+          "rviz_render_node");
 
-    auto mainWindow = std::make_shared<MainWindow>(&app, ros_node_abs);
-    mainWindow->show();
+  auto mainWindow = std::make_shared<MainWindow>(&app, ros_node_abs);
+  mainWindow->show();
 
-    while (rclcpp::ok()) {
-        app.processEvents();
-    }
+  while (rclcpp::ok()) {
+    app.processEvents();
+  }
 
-    return 0;
+  return 0;
 }
-
