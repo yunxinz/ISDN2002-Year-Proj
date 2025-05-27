ca0f5b769957f312641b2c84398ad2f93caa540a
yunxinz <zhang.yunxin@live.com>
2025-05-28 03:24:52 +0800

    add octomap server 2 submodule + edit params for max z filtering + launch script

--- octomap_server.sh
+++ octomap_server.sh
@@ -1,18 +1,7 @@
-ros2 run octomap_server octomap_server_node \
-  --ros-args \
-   # grid size = 5cm
-   # decrease for more precision, 
-   # increase for faster processing
-  -p resolution:=0.05 \
-   # ignore points above z = 2.0m   
-  -p max_height:=2.0 \ 
-  -p frame_id:=camera_init \
-  -p sensor_model.max_range:=10.0 \
-  -p publish_2d_projection:=true \
-  -p height_map:=true \
-  # points below z = 0.2m are considered ground
-  # relative to the ground plane (z = 0) not Lidar position 
-  -p ground_filter_distance:=0.2 \
-  -p ground_filter_min_points:=10 \
-  -r cloud_in:=/cloud_registered \
-  -r projected_map:=/octomap_2d
+# NOTE - correct height filtering noew, but wrong orientation of the lidar thus grid map
+
+
+colcon build --symlink-install --packages-select octomap_msgs octomap_server2
+source src/yp_sensors/octomap_server2/install/setup.bash
+ros2 launch octomap_server2 octomap_server_launch.py pointcloud_min_z:=0.0 pointcloud_max_z:=1.0
+
